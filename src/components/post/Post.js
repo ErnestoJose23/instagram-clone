446eb4513a9f9ef3c2659d1b308b6f869a53f2b5
Ernesto Custodio <ernestojosecg@gmail.com>
2020-08-24 13:20:54 +0200

    Post

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -6,8 +6,10 @@ import Avatar from "@material-ui/core/Avatar";
 function Post() {
   return (
     <div className="post">
-      <h3>Username</h3>
-
+      <div className="postHeader">
+        <Avatar className="avatar" alt="Yet" src="" />
+        <h4>Username</h4>
+      </div>
       <img
         className="postImage"
         src="https://tecnovortex.com/wp-content/uploads/2019/04/wallpaper-engine.jpg"
