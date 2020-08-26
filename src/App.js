c871dc29495ed3d897d01da6f28493720fcd275a
Ernesto Custodio <ernestojosecg@gmail.com>
2020-08-26 10:59:30 +0200

    Post firebase

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Post from "./components/post/Post";
 
 import "./App.css";
 import { db } from "./firebase";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "James",
-      caption: "Wallpaper",
-      imageUrl:
-        "https://tecnovortex.com/wp-content/uploads/2019/04/wallpaper-engine.jpg",
-    },
-    {
-      username: "John",
-      caption: "This is a wallpaper ",
-      imageUrl:
-        "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection("posts").onSnapshot(snapshot);
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(snapshot.docs.map((doc) => doc.data()));
+    });
   }, []);
 
   return (
@@ -41,17 +30,6 @@ function App() {
           imageUrl={post.imageUrl}
         />
       ))}
-
-      <Post
-        username="Curtis"
-        caption="Hello there!"
-        imageUrl="https://i.blogs.es/d24b49/studio-muti-wallpapers/450_1000.jpg"
-      />
-      <Post
-        username="David"
-        caption="General Kenobi"
-        imageUrl="https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg"
-      />
     </div>
   );
 }
