6b971c7506d1d975dddfb89e864392afd4fd69da
Ernesto Custodio <ernestojosecg@gmail.com>
2020-08-28 11:37:59 +0200

    Image Upload

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ImageUpload() {
+  const [caption, setCaption] = useState("");
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [ulr, setUrl] = useState("");
+
   return (
     <div>
       <h2>hola</h2>
+      <input
+        type="text"
+        placefolder="Enter a caption..."
+        value={caption}
+        onChange={(event) => setCaption(event.target.value)}
+      ></input>
+      <input type="file" onChange={handleChange}></input>
+      <Button onClick={handleUpload}>Upload</Button>
     </div>
   );
 }
