6ad026abf0a0e2ea7da6e1ee4e8dca1cdc8998fe
Ernesto Custodio <ernestojosecg@gmail.com>
2020-09-02 12:08:14 +0200

    Post

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Post.css";
+import { db } from "../../firebase";
 
 import Avatar from "@material-ui/core/Avatar";
 
-function Post({ username, caption, imageUrl }) {
+function Post({ postId, username, caption, imageUrl }) {
+  const [comment, setComments] = useState([]);
+
+  useEffect(() => {
+    let unsubscribe;
+    if (postId) {
+      unsubscribe = db
+        .collection("posts")
+        .doc(postId)
+        .collection("comments")
+        .onSnapshot((snapshot) => {
+          setComments(snapshot.docs.map((doc) => doc.data()));
+        });
+    }
+  });
+
   return (
     <div className="post">
       <div className="postHeader">
